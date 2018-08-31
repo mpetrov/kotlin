ce66da31933a19d367a662db6138f2098231f343
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2018-08-31 14:11:56 +0300

    Update testData for bytecode listing tests

--- compiler/testData/codegen/bytecodeText/inlineClasses/inlineClassExtendsErasedClass.kt
+++ compiler/testData/codegen/bytecodeText/inlineClasses/inlineClassExtendsErasedClass.kt
@@ -6,10 +6,10 @@ interface IFoo
 interface IBar
 inline class FooBar(val x: Int) : IFoo, IBar
 
-// 1 public final class Z extends Z\$Erased
-// 1 static class Z\$Erased
-// 0 public static class Z\$Erased
+// 1 public final class Z extends Z\-Erased
+// 1 static class Z\-Erased
+// 0 public static class Z\-Erased
 
-// 1 public final class FooBar extends FooBar\$Erased  implements IFoo IBar
-// 1 static class FooBar\$Erased
-// 0 public static class FooBar\$Erased
+// 1 public final class FooBar extends FooBar\-Erased  implements IFoo IBar
+// 1 static class FooBar\-Erased
+// 0 public static class FooBar\-Erased
